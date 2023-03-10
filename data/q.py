4481bd22fddeb1a6993e261e76dd90715d1ca760
M-itti <mahdlinux@gmail.com>
2023-03-10 05:59:30 +0330

    all changes

---
+++ data/q.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+
+# Read in the CSV file
+df = pd.read_csv("raw_train.csv")
+
+# List of columns to drop
+cols_to_drop = ['Id', 'OverallQual', 'YearBuilt', 'YearRemodAdd', 'BsmtFinType1_Unf', 
+                'HasWoodDeck', 'HasOpenPorch', 'HasEnclosedPorch', 'Has3SsnPorch', 
+                'HasScreenPorch', 'YearsSinceRemodel', 'Total_Home_Quality', 'LotFrontage', 
+                'LotArea', 'OverallCond', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 
+                'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea', 
+                'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 
+                'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 'GarageYrBlt', 'GarageCars', 
+                'GarageArea', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 
+                'ScreenPorch', 'PoolArea', 'MiscVal']
+
+
+# Drop the specified columns and print the resulting dataframe
+df.drop(cols_to_drop, axis=1, inplace=True)
+print(df.columns)
+
